79f711bd639d05c0fc45c558627d8985f7cc350e
Nestor_Gorrotxategi <ngorrotxa@gmail.com>
2022-12-03 18:53:05 +0100

    project 1

--- lab-project1/src/main.c
+++ lab-project1/src/main.c
@@ -1,4 +1,4 @@
-/***********************************************************************
+/*****************************************************************************************
  * Teammates: 
  *    NESTOR GORROTXATEGI AGIRRE
  *    DRAGOS BRATFALEAN
@@ -8,12 +8,12 @@
  * Read ADC signals from Joystick, read signal from rotary encoder and both
  * push buttons and display on LCD screen results.
  * 
- * Application: Control of direction and acceleration of a system. Joystick
+ * Application: Control of direction and acceleration/deceleration of a system. Joystick
  *              is used to control direction. When Joystick's push button is
  *              pressed, a buzzer activates.
  *              The rotary encoder is used to control acceleration and braking.
  *              When encoder's push button is pressed, the rear camera activates.
- **********************************************************************/
+ ********************************************************************************************/
 
 /* Defines ----------------------------------------------------------*/
 
@@ -41,6 +41,7 @@
  *           
  * Returns:  none
  **********************************************************************/
+
 int main(void)
 {
     // Initialize display
@@ -72,7 +73,7 @@ int main(void)
     TIM1_overflow_interrupt_enable();
 
     //Configure 16-bit Timer/Counter2 for the Encoder
-    //We set prescaler to 33ms and enable overflow interrupt
+    //We set prescaler to 4ms and enable overflow interrupt
     TIM2_overflow_4ms();
     TIM2_overflow_interrupt_enable();
 
@@ -90,10 +91,15 @@ int main(void)
 /**********************************************************************
  * Function: Timer/Counter1 overflow interrupt
  * Purpose:  Use single conversion mode and start conversion every 100 ms.
+ *           Switch ADC channels from A0 to A1 to read both ADC values.
  **********************************************************************/
+
 ISR(TIMER1_OVF_vect)
 {
+    //Definition of OVFs to start conversion every 100ms
     static uint8_t no_of_overflows = 0;
+    
+    //Definition of variable caso to create a condition to switch ADC channels
     static uint8_t caso = 0;
 
     no_of_overflows++;
@@ -102,106 +108,108 @@ ISR(TIMER1_OVF_vect)
       //Select channel
       if(caso==0){
       caso=1;
+      //Channel A0 selected
       ADMUX = ADMUX & ~(1<<MUX3 | 1<<MUX2 | 1<<MUX1 | 1<<MUX0);
       }else{
       caso=0;
+      //Channel A1 selected
       ADMUX = ADMUX | (1<<MUX0);
       }
     
       // Start ADC conversion
       ADCSRA = ADCSRA | (1<<ADSC);
       no_of_overflows= 0;
-    }
-    
+    }  
 }
 
 /* Interrupt service routines ----------------------------------------*/
 /**********************************************************************************
  * Function: Timer/Counter2 overflow interrupt
- * Purpose: Every 200ms, read encoder's and push buttons' values and display them
+ * Purpose: Every 4ms, read encoder's and push buttons' values and display them
  *          on the LCD screen according to application.
- *          Timer has been set to 200ms for one reason: ADC conversion is started
- *          every 100ms, and gets both X and Y axis values of Joystick every 200ms. 
- *          So, we are interested to read values from encoder and push buttons
- *          only when we have both X and Y values, that is, every 200ms.
+ *          Timer has been set to 4ms for one reason: Time of pulse of the encoder
+ *          is estimated 10ms. If a Timer is used which overflows in more than 10ms,
+ *          we can lose changes of state of the encoder.
  *********************************************************************************/
+
 ISR(TIMER2_OVF_vect)
 {
-
     //Encoder definitions
     static uint8_t aLastState = 0;
     static uint8_t aState=0;
     static uint8_t bState=0;
-    char string1[4];
-    char string2[4];
+
     //Push button definitions
     uint8_t Push_Joystick;
     uint8_t Push_Encoder;
 
-    //Timer2 OVFs every 16ms, so we need approximately 12 OVFs
-    //to reach 200ms.
 
-      //Read push button values
-      Push_Joystick = GPIO_read(&PINB, JOYSTICK_PUSH);
-      Push_Encoder = GPIO_read(&PINB, ENCODER_PUSH);
+    //Read push button values
+    Push_Joystick = GPIO_read(&PINB, JOYSTICK_PUSH);
+    Push_Encoder = GPIO_read(&PINB, ENCODER_PUSH);
 
-      if (Push_Joystick == 0){
-            //Actions to be done if buttons are pushed
-             lcd_clrscr();
-             lcd_gotoxy(1,6);
-             lcd_puts("BUZZER");
-      }else if(Push_Encoder == 0){       
-             lcd_clrscr();
-             lcd_gotoxy(1,6);
-             lcd_puts("REAR CAMERA");   
-      }else{
+    //Actions to be done if buttons are pushed
+    if (Push_Joystick == 0){
+      lcd_clrscr();
+      lcd_gotoxy(1,6);
+      lcd_puts("BUZZER");
+    }else if(Push_Encoder == 0){       
+      lcd_clrscr();
+      lcd_gotoxy(1,6);
+      lcd_puts("REAR CAMERA");   
+    }else{
 
-        //Loop to know in which direction is encoder turning
-        aState = GPIO_read(&PINB, ENCODER_OA);
+      //Loop to know in which direction encoder is turning
+      aState = GPIO_read(&PINB, ENCODER_OA);
 
-        // If the previous and the current state of the outputA are different, that means a pulse has occured.
-        if(aLastState != aState){
-            lcd_clrscr();
-            bState = GPIO_read(&PINB, ENCODER_OB);
-            // Let's compare a and b states
-           if(aState != bState){
-              //If a and b states are different, this means encoder is rotating clockwise.
-              lcd_gotoxy(1, 1);
-              lcd_puts("Accelerate");
-            }else{
-              //Encoder is rotating counter clockwise.
-              lcd_gotoxy(1, 1);
-              lcd_puts("Deccelerate");   
-            }
-          aLastState = aState;
-        }  
+      // If the previous and the current state of the outputA are different, that means a pulse has occured.
+      if(aLastState != aState){
+        lcd_clrscr();
+        bState = GPIO_read(&PINB, ENCODER_OB);
       
-      }
-    
+        // Let's compare a and b states
+        if(aState != bState){
+          //If a and b states are different, this means encoder is rotating clockwise.
+          lcd_gotoxy(1, 1);
+          lcd_puts("Accelerate");
+        }else{
+          //Encoder is rotating counter clockwise.
+          lcd_gotoxy(1, 1);
+          lcd_puts("Deccelerate");   
+        }
+        aLastState = aState;
+      }  
+      
+    }
 }
 
 /**********************************************************************
  * Function: ADC complete interrupt
- * Purpose:  Display converted value on LCD screen.
+ * Purpose:  Display converted values on LCD screen.
  **********************************************************************/
+
 ISR(ADC_vect)
 {
+    //Definitions of variable no_of_conversions. We want to process X and Y values
+    //every 2 conversions
     static uint8_t no_of_conversions = 0; 
     
-    //we will define two parameters to save joystick X and Y axis positions
+    //wW will define two parameters to save joystick X and Y axis positions
     static uint16_t Vrx=0;
     static uint16_t Vry=0;
 
     //We have 2 ADC pins connected, but AVR MCU can only make one conversion each ISR.
     //This means we cannot read both A0 and A1 values simultaneously. Therefore, we need to
-    //use a condition to read both values.
+    //use a condition to save both values.
     static uint8_t caso=0;
 
     if(caso == 0){
       caso=1;
+      //Saving X axis value
       Vrx = ADC;
     }else{
       caso=0;
+      //Saving Y axis value
       Vry = ADC;
     }
 
@@ -235,6 +243,4 @@ ISR(ADC_vect)
         lcd_puts("Right");
       }
     }
-
-
 }
